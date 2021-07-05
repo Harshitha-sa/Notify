83081e18dfcd27d4f2d235a005bd75766e6e94fd
Harshitha-sa <saharshithasa@gmail.com>
2021-07-05 06:04:03 +0530

    added image in notes

--- app/src/main/java/com/example/notify/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/notify/activities/CreateNoteActivity.java
@@ -1,13 +1,24 @@
 package com.example.notify.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -20,6 +31,7 @@ import com.example.notify.database.NotesDatabase;
 import com.example.notify.entities.Note;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -32,6 +44,10 @@ public class CreateNoteActivity extends AppCompatActivity {
     ImageView imageSave;
     String selectNoteColor;
     View viewSubtitleIndicator;
+    ImageView imageNote;
+    String selectedImagePath;
+    private static final int REQUEST_CODE_STORAGE_PERMISSION=1;
+    private static final int REQUEST_CODE_SELECT_IMAGE=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +70,8 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
         selectNoteColor="#333333";
+        selectedImagePath="";
+
         initiMiscellaneous();
         setSubtitleIndicatorColor();
     }
@@ -73,6 +91,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         note.setNoteText(inputNoteText.getText().toString());
         note.setDateTime(textDateTime.getText().toString());
         note.setColor(selectNoteColor);
+        note.setImagePath(selectedImagePath);
 
 
         @SuppressLint("StaticFieldLeak")
@@ -178,7 +197,23 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
 
-
+        layoutMiscellaneous.findViewById(R.id.layoutAddImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                if (ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
+                )!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            CreateNoteActivity.this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_STORAGE_PERMISSION
+                    );
+                }else {
+                    selectImage();
+                }
+            }
+        });
 
 
 
@@ -190,6 +225,62 @@ public class CreateNoteActivity extends AppCompatActivity {
         gradientDrawable.setColor(Color.parseColor(selectNoteColor));
 
     }
+
+    private void selectImage() {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUEST_CODE_STORAGE_PERMISSION&&grantResults.length>0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                selectImage();
+            }else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE_SELECT_IMAGE&&resultCode==RESULT_OK){
+            if (data!=null){
+                Uri selectedImageUri =data.getData();
+                if(selectedImageUri!=null){
+                    try {
+                        InputStream inputStream=getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+                        imageNote.setImageBitmap(bitmap);
+                        imageNote.setVisibility(View.VISIBLE);
+                        selectedImagePath=getPathFromUri(selectedImageUri);
+
+                    }catch (Exception exception){
+                        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private String getPathFromUri(Uri contentUri){
+        String filePath;
+        Cursor cursor=getContentResolver()
+                .query(contentUri,null,null,null,null);
+        if (cursor==null){
+            filePath=contentUri.getPath();
+        }else {
+            cursor.moveToFirst();
+            int index=cursor.getColumnIndex("_data");
+            filePath=cursor.getString(index);
+            cursor.close();
+        }
+        return filePath;
+    }
     private void initializefields() {
         imageBack=findViewById(R.id.image_back);
         inputNoteTitle=findViewById(R.id.inputNoteTitle);
@@ -198,5 +289,6 @@ public class CreateNoteActivity extends AppCompatActivity {
         textDateTime=findViewById(R.id.textDateTime);
         imageSave=findViewById(R.id.saveImage);
         viewSubtitleIndicator=findViewById(R.id.viewSubtitleIndicator);
+        imageNote=findViewById(R.id.imageNote);
     }
 }
--- app/src/main/java/com/example/notify/adapters/NotesAdapter.java
+++ app/src/main/java/com/example/notify/adapters/NotesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.notify.adapters;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.notify.R;
 import com.example.notify.entities.Note;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.List;
 
@@ -50,13 +52,14 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
     static class NotesViewHolder extends RecyclerView.ViewHolder{
         TextView textTitle,textSubtitle,textDateTime;
         LinearLayout layoutNote;
+        RoundedImageView imageNote;
         NotesViewHolder(@NonNull View itemView) {
             super(itemView);
             textTitle=itemView.findViewById(R.id.textTitle);
             textSubtitle=itemView.findViewById(R.id.textSubtitle);
             textDateTime=itemView.findViewById(R.id.textDateTime);
             layoutNote=itemView.findViewById(R.id.layoutNote);
-
+            imageNote=itemView.findViewById(R.id.imageNote);
         }
         void setNote(Note note){
             textTitle.setText(note.getTitle());
@@ -71,6 +74,12 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
                gradientDrawable.setColor(Color.parseColor(note.getColor()));
             }else{
                 gradientDrawable.setColor(Color.parseColor("#333333"));
+            }
+            if(note.getImagePath()!=null){
+                imageNote.setImageBitmap(BitmapFactory.decodeFile(note.getImagePath()));
+                imageNote.setVisibility(View.VISIBLE);
+            }else {
+                imageNote.setVisibility(View.GONE);
             }
         }
     }
